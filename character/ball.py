f8cfd6f62ec34b1745440517a87778de32c21531
xing-he888 <2835584575@qq.com>
2026-04-13 10:55:13 +0800

    sss

--- character/ball.py
+++ character/ball.py
@@ -5,6 +5,8 @@ class Ball:
         self.x = x
         self.y = y
         self.radius = radius
+        self.image=pygame.image.load("image/bullet_image/bullet_1.png").convert_alpha()
+        self.angle=0
         dx = target_x - x
         dy = target_y - y
         distance = math.sqrt(dx ** 2 + dy ** 2)
@@ -24,5 +26,16 @@ class Ball:
         return (self.x < 0 or self.x > screen_width or self.y < 0 or self.y > screen_height)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, (255, 0, 0), (int(self.x), int(self.y)), self.radius)
+        rotated_image = pygame.transform.rotate(self.image, self.angle)
+        rect = rotated_image.get_rect(center=(self.x, self.y))  # 获取旋转后图片的矩形，以玩家位置为中心
+        screen.blit(rotated_image, rect.topleft)  # 使用矩形左上角坐标绘制，确保旋转后不偏移
 
+
+    def get_pos(self):
+        return self.x, self.y
+
+    def move_rowards_play(self):
+        mx,my=pygame.mouse.get_pos()
+        dx=my-self.x
+        dy=my-self.y
+        self.angle=math.degrees(math.atan2(-dy,dx))-90
--- main.py
+++ main.py
@@ -2,7 +2,12 @@ import pygame
 from character import ball
 from character import player
 from character import enemy
+import time
+import os
+
+
 def main():
+    os.chdir(os.path.dirname(os.path.abspath(__file__)))
     pygame.init()
 
     screen = pygame.display.set_mode((800, 600))
@@ -29,12 +34,14 @@ def main():
         keys = pygame.key.get_pressed()
 
         enemy1.move_towards_player(player1.x, player1.y)
+
         # 创建人物
         player1.move(keys, 800, 600)
         player1.draw(screen)
 
-        # 创建小球
+        # 遍历小球
         for b in balls[:]:
+            b.move_rowards_play()
             b.move()
             b.draw(screen)
             dx = b.x - enemy1.x
@@ -42,6 +49,7 @@ def main():
             distance = (dx ** 2 + dy ** 2) ** 0.5
             if distance < (b.radius + enemy1.size):
                 if enemy1.attack(1):
+                    # time.sleep(1)
                     enemy1 = enemy.Enemy("image/tank_image/tank_1.png", 20, 10)
                 balls.remove(b)
                 continue
@@ -52,11 +60,10 @@ def main():
         # 创建敌人
         enemy1.draw(screen)
 
-        fps.tick(120)
+        fps.tick(144)
         pygame.display.flip()
 
     pygame.quit()
 
 
 #此页面用于测试
-
